25fbf3180c89330fda132f4f5dd7e46b73c4d016
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-02-24 16:30:20 -0300

    Calculadora para converter moeda
    Convertendo um valor em dólar para real. Declarando as variáveis que serão usadas nas operações e convertendo os valores para decimal quando necessário para poder ajudar os cálculos e por fim, imprimindo o valor convertido na tela.

---
+++ converterMoeda/converterMoeda/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace converterMoeda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDolar = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtReal = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnRecalcular = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dólar ($):";
+            // 
+            // txtDolar
+            // 
+            this.txtDolar.Location = new System.Drawing.Point(108, 34);
+            this.txtDolar.Name = "txtDolar";
+            this.txtDolar.Size = new System.Drawing.Size(110, 20);
+            this.txtDolar.TabIndex = 1;
+            this.txtDolar.TextChanged += new System.EventHandler(this.txtDolar_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Real (R$):";
+            // 
+            // txtReal
+            // 
+            this.txtReal.Location = new System.Drawing.Point(108, 72);
+            this.txtReal.Name = "txtReal";
+            this.txtReal.Size = new System.Drawing.Size(110, 20);
+            this.txtReal.TabIndex = 3;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.Color.Transparent;
+            this.btnCalcular.Location = new System.Drawing.Point(38, 116);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnRecalcular
+            // 
+            this.btnRecalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnRecalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRecalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRecalcular.ForeColor = System.Drawing.Color.Transparent;
+            this.btnRecalcular.Location = new System.Drawing.Point(119, 116);
+            this.btnRecalcular.Name = "btnRecalcular";
+            this.btnRecalcular.Size = new System.Drawing.Size(99, 23);
+            this.btnRecalcular.TabIndex = 5;
+            this.btnRecalcular.Text = "Recalcular";
+            this.btnRecalcular.UseVisualStyleBackColor = false;
+            this.btnRecalcular.Click += new System.EventHandler(this.btnRecalcular_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(325, 55);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(260, 55);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "Faça um programa que: - Leia a cotação do dólar - Leia um valor em dólares - Conv" +
+    "erta esse valor para Real - Mostre o resultado";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 184);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.btnRecalcular);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtReal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDolar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDolar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtReal;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnRecalcular;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ converterMoeda/converterMoeda/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace converterMoeda
+{
+    public partial class Form1 : Form
+    {
+        decimal valor1 =0, valorConvertido = 0;
+        double taxa = 5.75;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (txtDolar.Text != "")
+            {
+                valor1 = decimal.Parse(txtDolar.Text, CultureInfo.InvariantCulture);
+                valorConvertido = valor1 * (decimal)taxa;
+                txtReal.Text = Convert.ToString(valorConvertido);
+            }
+            else
+            {
+                MessageBox.Show("Digite uma valor para realizar o calculo.");
+            }
+        }
+
+        private void btnRecalcular_Click(object sender, EventArgs e)
+        {
+            txtDolar.Text = "";
+            txtDolar.Focus();
+            txtReal.Text = "";
+        }
+
+        private void txtDolar_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
