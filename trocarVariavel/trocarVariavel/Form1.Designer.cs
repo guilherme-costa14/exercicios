70acd6cb951f843f0ecfdb615271577bb7a6b3ff
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-02-24 17:15:00 -0300

    Trocar valores
    Trocar os valores de variáveis para elas assumirem um o valor da outra...

---
+++ trocarVariavel/trocarVariavel/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace trocarVariavel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtValorA = new System.Windows.Forms.TextBox();
+            this.txtValorB = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtValorATrocado = new System.Windows.Forms.TextBox();
+            this.txtValorBTrocado = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnTrocar = new System.Windows.Forms.Button();
+            this.btnRecalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(483, 31);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(224, 77);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(70, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Digite os valores";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Valor A:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Valor B:";
+            // 
+            // txtValorA
+            // 
+            this.txtValorA.Location = new System.Drawing.Point(86, 58);
+            this.txtValorA.Name = "txtValorA";
+            this.txtValorA.Size = new System.Drawing.Size(100, 20);
+            this.txtValorA.TabIndex = 4;
+            // 
+            // txtValorB
+            // 
+            this.txtValorB.Location = new System.Drawing.Point(86, 88);
+            this.txtValorB.Name = "txtValorB";
+            this.txtValorB.Size = new System.Drawing.Size(100, 20);
+            this.txtValorB.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(232, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Valor A:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(232, 89);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Valor B:";
+            // 
+            // txtValorATrocado
+            // 
+            this.txtValorATrocado.Location = new System.Drawing.Point(292, 58);
+            this.txtValorATrocado.Name = "txtValorATrocado";
+            this.txtValorATrocado.Size = new System.Drawing.Size(100, 20);
+            this.txtValorATrocado.TabIndex = 8;
+            // 
+            // txtValorBTrocado
+            // 
+            this.txtValorBTrocado.Location = new System.Drawing.Point(292, 88);
+            this.txtValorBTrocado.Name = "txtValorBTrocado";
+            this.txtValorBTrocado.Size = new System.Drawing.Size(100, 20);
+            this.txtValorBTrocado.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(268, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(110, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Valores trocados";
+            // 
+            // btnTrocar
+            // 
+            this.btnTrocar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnTrocar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTrocar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTrocar.ForeColor = System.Drawing.Color.Transparent;
+            this.btnTrocar.Location = new System.Drawing.Point(134, 133);
+            this.btnTrocar.Name = "btnTrocar";
+            this.btnTrocar.Size = new System.Drawing.Size(75, 23);
+            this.btnTrocar.TabIndex = 11;
+            this.btnTrocar.Text = "Trocar";
+            this.btnTrocar.UseVisualStyleBackColor = false;
+            this.btnTrocar.Click += new System.EventHandler(this.btnTrocar_Click);
+            // 
+            // btnRecalcular
+            // 
+            this.btnRecalcular.BackColor = System.Drawing.Color.Red;
+            this.btnRecalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRecalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRecalcular.ForeColor = System.Drawing.Color.Transparent;
+            this.btnRecalcular.Location = new System.Drawing.Point(215, 133);
+            this.btnRecalcular.Name = "btnRecalcular";
+            this.btnRecalcular.Size = new System.Drawing.Size(107, 23);
+            this.btnRecalcular.TabIndex = 12;
+            this.btnRecalcular.Text = "Recalcular";
+            this.btnRecalcular.UseVisualStyleBackColor = false;
+            this.btnRecalcular.Click += new System.EventHandler(this.btnRecalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 203);
+            this.Controls.Add(this.btnRecalcular);
+            this.Controls.Add(this.btnTrocar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtValorBTrocado);
+            this.Controls.Add(this.txtValorATrocado);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtValorB);
+            this.Controls.Add(this.txtValorA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtValorA;
+        private System.Windows.Forms.TextBox txtValorB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtValorATrocado;
+        private System.Windows.Forms.TextBox txtValorBTrocado;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnTrocar;
+        private System.Windows.Forms.Button btnRecalcular;
+    }
+}
+
---
+++ trocarVariavel/trocarVariavel/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trocarVariavel
+{
+    public partial class Form1 : Form
+    {
+        decimal valorA = 0, valorB = 0, valorC = 0;
+
+        private void btnRecalcular_Click(object sender, EventArgs e)
+        {
+            txtValorA.Text = "";
+            txtValorB.Text = "";
+            txtValorATrocado.Text = "";
+            txtValorBTrocado.Text = "";
+            txtValorA.Focus();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTrocar_Click(object sender, EventArgs e)
+        {
+            if(txtValorA.Text != "" && txtValorB.Text != "")
+            {
+                valorA = decimal.Parse(txtValorA.Text, CultureInfo.InvariantCulture);
+                valorB = decimal.Parse(txtValorB.Text, CultureInfo.InvariantCulture);
+
+                valorC = valorA;
+                valorA = valorB;
+                valorB = valorC;
+
+                txtValorATrocado.Text = Convert.ToString(valorA);
+                txtValorBTrocado.Text = Convert.ToString(valorB);
+            }
+            else
+            {
+                MessageBox.Show("Digite os valores para efetuar as trocas.");
+            }
+        }
+    }
+}
