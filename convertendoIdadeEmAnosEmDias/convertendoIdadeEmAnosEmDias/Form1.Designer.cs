afc0e12ed0011e2ab2ba85a60d3bf4eabb5456cb
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-02-24 16:54:50 -0300

    Convertendo idade em anos para somente em dia
    Escrevendo um código para transformar a idade apenas em anos, meses e dias para dias. Os valores de anos foram multiplicados por valores definidos no exercício e somente o dia foi somado ao resultado final. Definindo o botão calcular para efetuar as operações e o recalcular permite que o usuário recalcule sem ter que abrir um forms novo.

---
+++ convertendoIdadeEmAnosEmDias/convertendoIdadeEmAnosEmDias/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace convertendoIdadeEmAnosEmDias
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnRecalcular = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAnos = new System.Windows.Forms.TextBox();
+            this.txtMeses = new System.Windows.Forms.TextBox();
+            this.txtDias = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(328, 84);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(234, 77);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e" +
+    " escreva a idade dessa pessoa expressa apenas em dias. Considerar ano com 365 di" +
+    "as e mês com 30 dias.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(77, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Anos:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(67, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Meses:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(80, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Dias:";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.Color.Transparent;
+            this.btnCalcular.Location = new System.Drawing.Point(69, 203);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnRecalcular
+            // 
+            this.btnRecalcular.BackColor = System.Drawing.Color.Red;
+            this.btnRecalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRecalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRecalcular.ForeColor = System.Drawing.Color.Transparent;
+            this.btnRecalcular.Location = new System.Drawing.Point(150, 203);
+            this.btnRecalcular.Name = "btnRecalcular";
+            this.btnRecalcular.Size = new System.Drawing.Size(101, 23);
+            this.btnRecalcular.TabIndex = 5;
+            this.btnRecalcular.Text = "Reculcular";
+            this.btnRecalcular.UseVisualStyleBackColor = false;
+            this.btnRecalcular.Click += new System.EventHandler(this.btnRecalcular_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(102, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(149, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Converter anos em dias";
+            // 
+            // txtAnos
+            // 
+            this.txtAnos.Location = new System.Drawing.Point(124, 52);
+            this.txtAnos.Name = "txtAnos";
+            this.txtAnos.Size = new System.Drawing.Size(105, 20);
+            this.txtAnos.TabIndex = 7;
+            // 
+            // txtMeses
+            // 
+            this.txtMeses.Location = new System.Drawing.Point(124, 90);
+            this.txtMeses.Name = "txtMeses";
+            this.txtMeses.Size = new System.Drawing.Size(105, 20);
+            this.txtMeses.TabIndex = 8;
+            // 
+            // txtDias
+            // 
+            this.txtDias.Location = new System.Drawing.Point(124, 127);
+            this.txtDias.Name = "txtDias";
+            this.txtDias.Size = new System.Drawing.Size(105, 20);
+            this.txtDias.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(46, 165);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Resultado:";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(124, 164);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(105, 20);
+            this.txtResultado.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 250);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDias);
+            this.Controls.Add(this.txtMeses);
+            this.Controls.Add(this.txtAnos);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnRecalcular);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnRecalcular;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAnos;
+        private System.Windows.Forms.TextBox txtMeses;
+        private System.Windows.Forms.TextBox txtDias;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtResultado;
+    }
+}
+
---
+++ convertendoIdadeEmAnosEmDias/convertendoIdadeEmAnosEmDias/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace convertendoIdadeEmAnosEmDias
+{
+    public partial class Form1 : Form
+    {
+        decimal idadeEmDias = 0, anos = 365, dias = 30, valorEmAnos = 0, valorEmMeses = 0, valorEmDias = 0;
+
+        private void btnRecalcular_Click(object sender, EventArgs e)
+        {
+            txtAnos.Text = "";
+            txtMeses.Text = "";
+            txtDias.Text = "";
+            txtResultado.Text = "";
+            txtAnos.Focus();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (txtAnos.Text != "" && txtMeses.Text != "" && txtDias.Text != "")
+            {
+                valorEmAnos = decimal.Parse(txtAnos.Text, CultureInfo.InvariantCulture);
+                valorEmMeses = decimal.Parse(txtMeses.Text, CultureInfo.InvariantCulture);
+                valorEmDias = decimal.Parse(txtDias.Text, CultureInfo.InvariantCulture);
+
+                idadeEmDias = (valorEmAnos * anos) + (valorEmMeses * dias) + valorEmDias;
+
+                txtResultado.Text = Convert.ToString(idadeEmDias);
+            }
+            else
+            {
+                MessageBox.Show("Preencha os campos corretamente.");
+            }
+        }
+    }
+}
