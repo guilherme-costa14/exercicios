7c36e3f8a1cad56968dd3b2da0fee3dc3d454685
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-02-25 16:48:00 -0300

    Calculando IMC
    Corrigindo erros de lógica e tratamento de dados na hora da saída e durante o processamento do programa. Entendo melhor as estruturas de if e validando melhor campos nulos para não continuar com o calculo.

---
+++ imc/imc/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace imc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBoxMasculino = new System.Windows.Forms.CheckBox();
+            this.checkBoxFeminino = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPesoIdeal = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnRecalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(461, 48);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(215, 92);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(123, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Calculo de IMC";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(65, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Genêro:";
+            // 
+            // checkBoxMasculino
+            // 
+            this.checkBoxMasculino.AutoSize = true;
+            this.checkBoxMasculino.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxMasculino.Location = new System.Drawing.Point(126, 68);
+            this.checkBoxMasculino.Name = "checkBoxMasculino";
+            this.checkBoxMasculino.Size = new System.Drawing.Size(87, 20);
+            this.checkBoxMasculino.TabIndex = 3;
+            this.checkBoxMasculino.Text = "Masculino";
+            this.checkBoxMasculino.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxFeminino
+            // 
+            this.checkBoxFeminino.AutoSize = true;
+            this.checkBoxFeminino.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxFeminino.Location = new System.Drawing.Point(219, 68);
+            this.checkBoxFeminino.Name = "checkBoxFeminino";
+            this.checkBoxFeminino.Size = new System.Drawing.Size(81, 20);
+            this.checkBoxFeminino.TabIndex = 4;
+            this.checkBoxFeminino.Text = "Feminino";
+            this.checkBoxFeminino.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(96, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Altura (cm):";
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(175, 106);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 6;
+            this.txtAltura.TextChanged += new System.EventHandler(this.txtAltura_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(67, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Peso Ideal (Kg):";
+            // 
+            // txtPesoIdeal
+            // 
+            this.txtPesoIdeal.Location = new System.Drawing.Point(175, 147);
+            this.txtPesoIdeal.Name = "txtPesoIdeal";
+            this.txtPesoIdeal.Size = new System.Drawing.Size(100, 20);
+            this.txtPesoIdeal.TabIndex = 8;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.Color.Transparent;
+            this.btnCalcular.Location = new System.Drawing.Point(94, 189);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 36);
+            this.btnCalcular.TabIndex = 9;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnRecalcular
+            // 
+            this.btnRecalcular.BackColor = System.Drawing.Color.Red;
+            this.btnRecalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRecalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRecalcular.ForeColor = System.Drawing.Color.Transparent;
+            this.btnRecalcular.Location = new System.Drawing.Point(175, 189);
+            this.btnRecalcular.Name = "btnRecalcular";
+            this.btnRecalcular.Size = new System.Drawing.Size(99, 36);
+            this.btnRecalcular.TabIndex = 10;
+            this.btnRecalcular.Text = "Recalcular";
+            this.btnRecalcular.UseVisualStyleBackColor = false;
+            this.btnRecalcular.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRecalcular);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtPesoIdeal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBoxFeminino);
+            this.Controls.Add(this.checkBoxMasculino);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBoxMasculino;
+        private System.Windows.Forms.CheckBox checkBoxFeminino;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPesoIdeal;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnRecalcular;
+    }
+}
+
---
+++ imc/imc/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace imc
+{
+    public partial class Form1 : Form
+    {
+        decimal alturaCm = 0, alturaM = 0, pesoIdeal = 0;
+        double homem = 72.7, mulher = 62.1, correcaoMulher = 44.7;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtAltura.Text = "";
+            txtPesoIdeal.Text = "";
+
+            checkBoxFeminino.Checked = false;
+            checkBoxMasculino.Checked = false;
+
+            txtAltura.Focus();
+        }
+
+        private void txtAltura_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtAltura.Text))
+            {
+                if (decimal.TryParse(txtAltura.Text, NumberStyles.Float, CultureInfo.InvariantCulture, out decimal alturaConvertida))
+                {
+                    alturaCm = alturaConvertida;
+                    alturaM = alturaCm / 100;
+                }
+                else
+                {
+                    MessageBox.Show("Digite um valor numérico válido para a altura em cm.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (alturaM <= 0)
+            {
+                MessageBox.Show("Digite uma altura válida antes de calcular.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (checkBoxMasculino.Checked)
+            {
+                pesoIdeal = ((decimal)homem * alturaM) - 58;
+                txtPesoIdeal.Text = pesoIdeal.ToString("F2", CultureInfo.InvariantCulture);
+            }
+            else if (checkBoxFeminino.Checked)
+            {
+                pesoIdeal = ((decimal)mulher * alturaM) - (decimal)correcaoMulher;
+                txtPesoIdeal.Text = pesoIdeal.ToString("F2", CultureInfo.InvariantCulture);
+            }
+            else
+            {
+                MessageBox.Show("Você precisa escolher um gênero para calcular o peso ideal.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
