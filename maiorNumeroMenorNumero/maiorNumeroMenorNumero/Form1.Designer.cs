67f5ed476c12bc3345193306c42e6c08c18adbaf
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-02-25 14:42:35 -0300

    Verificando qual é o maior e o menor valor
    Usando operadores ternário para fazer essa verificação e corrigindo erro de lógica.

---
+++ maiorNumeroMenorNumero/maiorNumeroMenorNumero/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace maiorNumeroMenorNumero
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtValorA = new System.Windows.Forms.TextBox();
+            this.txtValorB = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMaiorValor = new System.Windows.Forms.TextBox();
+            this.txtMenorValor = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtIgual = new System.Windows.Forms.TextBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.btnRecalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(420, 57);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(225, 83);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(64, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Digite os valores";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Valor A:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(34, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Valor B:";
+            // 
+            // txtValorA
+            // 
+            this.txtValorA.Location = new System.Drawing.Point(94, 66);
+            this.txtValorA.Name = "txtValorA";
+            this.txtValorA.Size = new System.Drawing.Size(100, 20);
+            this.txtValorA.TabIndex = 4;
+            // 
+            // txtValorB
+            // 
+            this.txtValorB.Location = new System.Drawing.Point(94, 101);
+            this.txtValorB.Name = "txtValorB";
+            this.txtValorB.Size = new System.Drawing.Size(100, 20);
+            this.txtValorB.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(253, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Maior Valor";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(253, 101);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Menor Valor";
+            // 
+            // txtMaiorValor
+            // 
+            this.txtMaiorValor.Location = new System.Drawing.Point(241, 67);
+            this.txtMaiorValor.Name = "txtMaiorValor";
+            this.txtMaiorValor.Size = new System.Drawing.Size(100, 20);
+            this.txtMaiorValor.TabIndex = 8;
+            // 
+            // txtMenorValor
+            // 
+            this.txtMenorValor.Location = new System.Drawing.Point(241, 129);
+            this.txtMenorValor.Name = "txtMenorValor";
+            this.txtMenorValor.Size = new System.Drawing.Size(100, 20);
+            this.txtMenorValor.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(253, 165);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "São Iguais";
+            // 
+            // txtIgual
+            // 
+            this.txtIgual.Location = new System.Drawing.Point(230, 195);
+            this.txtIgual.Name = "txtIgual";
+            this.txtIgual.Size = new System.Drawing.Size(124, 20);
+            this.txtIgual.TabIndex = 11;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnVerificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVerificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerificar.ForeColor = System.Drawing.Color.Transparent;
+            this.btnVerificar.Location = new System.Drawing.Point(20, 134);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(80, 23);
+            this.btnVerificar.TabIndex = 12;
+            this.btnVerificar.Text = "Varificar";
+            this.btnVerificar.UseVisualStyleBackColor = false;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // btnRecalcular
+            // 
+            this.btnRecalcular.BackColor = System.Drawing.Color.Red;
+            this.btnRecalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRecalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRecalcular.ForeColor = System.Drawing.Color.Transparent;
+            this.btnRecalcular.Location = new System.Drawing.Point(106, 134);
+            this.btnRecalcular.Name = "btnRecalcular";
+            this.btnRecalcular.Size = new System.Drawing.Size(98, 23);
+            this.btnRecalcular.TabIndex = 13;
+            this.btnRecalcular.Text = "Recalcular";
+            this.btnRecalcular.UseVisualStyleBackColor = false;
+            this.btnRecalcular.Click += new System.EventHandler(this.btnRecalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 237);
+            this.Controls.Add(this.btnRecalcular);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.txtIgual);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtMenorValor);
+            this.Controls.Add(this.txtMaiorValor);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtValorB);
+            this.Controls.Add(this.txtValorA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtValorA;
+        private System.Windows.Forms.TextBox txtValorB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMaiorValor;
+        private System.Windows.Forms.TextBox txtMenorValor;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtIgual;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Button btnRecalcular;
+    }
+}
+
---
+++ maiorNumeroMenorNumero/maiorNumeroMenorNumero/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace maiorNumeroMenorNumero
+{
+    public partial class Form1 : Form
+    {
+        decimal valorA = 0, valorB = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRecalcular_Click(object sender, EventArgs e)
+        {
+            txtValorA.Text = "";
+            txtValorB.Text = "";
+            txtMaiorValor.Text = "";
+            txtMenorValor.Text = "";
+            txtIgual.Text = "";
+            txtValorA.Focus();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            valorA = decimal.Parse(txtValorA.Text, CultureInfo.InvariantCulture);
+            valorB = decimal.Parse(txtValorB.Text, CultureInfo.InvariantCulture);
+
+            if (valorA == valorB)
+            {
+                txtIgual.Text = Convert.ToString("Os valores são iguais");
+            }
+            else
+            {
+                decimal maior = (valorA > valorB) ? valorA : valorB;
+                decimal menor = (valorA < valorB) ? valorA : valorB;
+
+                txtMaiorValor.Text = maior.ToString(CultureInfo.InvariantCulture);
+                txtMenorValor.Text = menor.ToString(CultureInfo.InvariantCulture);
+            }
+
+        }
+    }
+}
